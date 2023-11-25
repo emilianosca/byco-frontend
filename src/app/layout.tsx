79775d69f01e920d77699270699d71740b8d7ebf
emilianosca <emiliano_sanchez@criptext.com>
2023-11-26 00:33:21 +0100

    Fixed bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Suspense fallback={<Loading />} >
+      {/* <Suspense fallback={<Loading />} > */}
         {children}
-      </Suspense>
+      {/* </Suspense> */}
       </body>
     </html>
   )
--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,47 +1,52 @@
-"use client";
-import React from "react";
+// "use client";
+import React, {  Suspense } from "react";
 import BoxQuestion from "@/components/boxQuestion";
 import getDummyQuestions, { getDummyQuestion } from "@/utils/getRandomQ";
 import getDate from "@/utils/getDate";
-import { useState, useEffect } from "react";
+import Loading from "@/app/loading";
 
-// export default function Questions({ params }: { params: { slug: string }  }) {
-export async function Questions() {
-  const [isMounted, setIsMounted] = useState(false);
-  // This is a side effect that runs after the first render and sets the isMounted state to true
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  const questions = getDummyQuestions();
+async function Question() {
   const data = getDummyQuestion();
-  // destructuring object
 
+  // destructuring object
   const question = data.question;
   const answersNumber = data.answersNumber;
   const createdAt = data.createdAt;
-  // change type of created At to Date
-  // const createdAtFormat = getDate(createdAt);
-
-  // console.log("type of createAt fromated", typeof createdAtFormat);
-
-  // This is a conditional rendering that returns null if the component is not mounted yet
-  if (!isMounted) {
-    return null;
-  }
 
   return (
     <>
-      <h1 className="text-black">¿Que piensas de esta pregunta?</h1>
-      <BoxQuestion 
-        question={question}
-        answersNumber={answersNumber}
-        createdAt={createdAt}
-      />
-      <pre className="text-black">{JSON.stringify(data, null, 2)}</pre>
+    <BoxQuestion
+      question={question}
+      answersNumber={answersNumber}
+      createdAt={createdAt}
+    />
+    {/* <pre className="text-black">{JSON.stringify(data, null, 2)}</pre> */}
     </>
   );
 }
 
 
-export default Questions;
+
+export async function Page() {
+  // const [isMounted, setIsMounted] = useState(false);
+  // // This is a side effect that runs after the first render and sets the isMounted state to true
+  // useEffect(() => {
+  //   setIsMounted(true);
+  // }, []);
+
+  // // This is a conditional rendering that returns null if the component is not mounted yet
+  // if (!isMounted) {
+  //   return null;
+  // }
+
+  return (
+    <>
+      <Suspense fallback={<p>Loading weather...</p>}>
+      <h1 className="text-black">¿Que piensas de esta pregunta?</h1>
+        <Question />
+      </Suspense>
+    </>
+  );
+}
+
+export default Page;
